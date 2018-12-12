4be092c866bff0df14f8e765459e13c06f62e831
jzhang3049 <ZJZ1983@wuhan>
2018-12-11 22:32:23 -0800

    updated

--- Fraction/src/Main.java
+++ Fraction/src/Main.java
@@ -15,53 +15,38 @@ public class Main{
     static class Fraction {
         private final int numerator;
         private final int denominator;
-        public Fraction(int numerator, int denominator) {
+        Fraction(int numerator, int denominator) {
             this.numerator = numerator;
             this.denominator = denominator;
         }
-
-        public double toDouble(Fraction x) {
-            double result = (double)numerator / (double)denominator;
-            return result;
+        double toDouble(Fraction x) {
+            return (double)numerator /(double)denominator;
         }
-
         Fraction plus(Fraction r) {
-            int product = this.denominator * r.denominator;
+            int new_denominator = this.denominator * r.denominator;
             int new_numerator = this.numerator * r.denominator + this.denominator * r.numerator;
-            int new_denominator = product;
             Fraction x = new Fraction(new_numerator, new_denominator);
             return x;
         }
-
         Fraction multiply(Fraction r) {
             int new_numerator = this.numerator * r.numerator;
             int new_denominator = this.denominator * r.denominator;
             Fraction x = new Fraction(new_numerator, new_denominator);
             return x;
         }
-
-        /* 将自己以“分子/分母”的形式输出到标准输出，并带有回车换行。如果分数是1/1，应该输出1。 当分子大于分母时，不需要提出整数部分，即31/30是一个正确的输出。 注意，在创建和做完运算后应该化简分数为最简形式。如2/4应该被化简为1/2。 */
-        void print() {
-            if (numerator == 1) {
-                if (denominator == 1)
-                    System.out.println("1");
-                else
-                    System.out.println("1" + "/" + denominator);
-            } else {
-                //求分子分母最大公约数
-                int k = 1;
-                int gcd = 1;
-                while (k <= numerator) {
-                    if ((numerator % k == 0) && (denominator % k == 0)) {
-                        gcd = k;
-                    }
-                    k++;
+        void print() {/* 输出“分子/分母”，并回车换行。当分子>分母，不要提出整数，即31/30是正确的。运算后应该约分。*/
+            int i = 1;
+            int j = 1;
+            while (i <= numerator) {
+                if ((numerator %i == 0) && (denominator %i == 0)) {
+                    j = i;
                 }
-                if (numerator / gcd == 1 && denominator / gcd == 1) {
-                    System.out.println("1");
-                } else
-                    System.out.println((numerator / gcd) + "/" + (denominator / gcd));
+                i++;
             }
+            if (numerator / j == 1 && denominator /j == 1) {
+                System.out.println("1");
+            } else
+                System.out.println((numerator /j) + "/" + (denominator /j));
         }
     }
 }
